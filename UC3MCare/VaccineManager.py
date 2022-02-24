44c5458def64d8b12dfc3475dfba5d5cc83a6bce
100429678 <100429678@alumnos.uc3m.es>
2022-02-24 17:54:55 +0100

    added validate GUID method and checked some errors

--- UC3MCare/VaccineManager.py
+++ UC3MCare/VaccineManager.py
@@ -1,4 +1,6 @@
 import json
+import uuid
+
 from .VaccineMangementException import VaccineManagementException
 from .VaccineRequest import VaccineRequest
 
@@ -6,9 +8,21 @@ class VaccineManager:
     def __init__(self):
         pass
 
-    def ValidateGUID( self, GUID ):
+    @staticmethod
+    def ValidateGUID( GUID ):
         # PLEASE INCLUDE HERE THE CODE FOR VALIDATING THE GUID
         # RETURN TRUE IF THE GUID IS RIGHT, OR FALSE IN OTHER CASE
+        try:
+            myUUID = uuid.UUID(GUID)
+            import re
+            myregex = re.compile(r'^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-'
+                                 r'[0-9A-F]{12}$'
+                                 , re.IGNORECASE)
+            x = myregex.fullmatch(GUID)
+            if not x:
+                raise VaccineManagementException("Invalid UUID v4 format")
+        except ValueError as e:
+            raise VaccineManagementException("Id receive is not a UUID") from e
         return True
 
     def ReadaccessrequestfromJSON(self, fi):
