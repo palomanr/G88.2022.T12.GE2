6fd0668e02b4894b44439166eebede6c2073de30
100429678 <100429678@alumnos.uc3m.es>
2022-02-25 14:14:23 +0100

    final changes for pylint exceptions

--- UC3M_Care/UC3M_VaccineManagementException.py
+++ UC3M_Care/UC3M_VaccineManagementException.py
@@ -1,13 +1,16 @@
+"""THIS IS THE VACCINE MANAGER EXCEPTION MODULE WHERE WE HANDLE EXCEPTIONS RELATED TO THE VACCINE MANAGER MODULE"""
+
 class VACCINEMANAGEMENTEXCEPTION(Exception):
+    """THIS IS THE CLASS WHERE WE HANDLE THE EXCEPTIONS MESSAGES"""
     def __init__(self, message):
-        self.__message = message
+        self.messageContent = message
         super().__init__(self.message)
 
     @property
     def message(self):
-        return self.__message
+        """This is the method used to send the messages shown for the exceptions"""
+        return self.messageContent
 
     @message.setter
     def message(self,value):
-        self.__message = value
-
+        self.messageContent = value
